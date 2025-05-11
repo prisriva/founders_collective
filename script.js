e3d8dac74261a591256daec075afd7ff5e122ec6
prisriva <prisriva004@gmail.com>
2025-05-11 19:02:45 -0400

    new

--- script.js
+++ script.js
@@ -63,3 +63,10 @@ if (menuToggle) {
     navLinks.classList.toggle("active");
   });
 }
+
+/* nav hamburger*/
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
